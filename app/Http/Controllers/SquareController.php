376cb440f95e35ca5eaf7be1c6db74e7feeabcd1
Anderson <andersonnj05@gmail.com>
2023-10-25 20:33:29 -0300

    consertando os erros

--- app/Http/Controllers/SquareController.php
+++ app/Http/Controllers/SquareController.php
@@ -19,10 +19,17 @@ class SquareController extends Controller
    //Cadastrando quadras
    public function store(Request $request)
    {
+        $request->validate([
+            'name' => 'required|string',
+            'total_units' => 'required|integer',
+            'polygon' => 'required|json',
+            'starting_point' => 'required|string'
+        ]);
+
         $square                 = new Square();
         $square->id             = $request->get('id');
         $square->name           = $request->get('name');
-        $square->user_id        = $request->auth()->user()->id;
+        $square->user_id        = auth()->user()->id;
         $square->total_units    = $request->get('total_units');
         $square->polygon        = $request->get('polygon');
         $square->starting_point = $request->get('starting_point');
@@ -43,5 +50,37 @@ class SquareController extends Controller
         }
 
         return response()->json($square);
+   }
+
+   public function update(int $id, Request $request)
+   {
+        $request->validate([
+            'name' => 'required|string',
+            'total_units' => 'required|integer',
+            'polygon' => 'required|json',
+            'starting_point' => 'required|string'
+        ]);
+
+        $square = Square::query()->where('user_id', Auth::user()->id)->find($id);
+        $square->name           = $request->get('name');
+        $square->total_units    = $request->get('total_units');
+        $square->polygon        = $request->get('polygon');
+        $square->starting_point = $request->get('starting_point');
+        $square->update();
+
+        return response()->json($square);
+   }
+
+   public function destroy(int $id)
+   {
+        $square = Square::query()->where('user_id', Auth::user()->id)->find($id);
+
+        if(!$square){
+            return response()->json(['message' => 'Quadra não encontada!'], 404);
+        }
+
+        $square->delete();
+
+        return response()->json(['message' => 'Quadra deletada com sucesso!'], 200);
    }
 }
